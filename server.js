3eeac0dd31e111e43546571abbb3758eff068ace
https://github.com/PJQ16/githubforDev <patipanjung97@gmail.com>
2024-03-11 10:22:35 +0700

    server update

--- server.js
+++ server.js
@@ -11,16 +11,15 @@ const port = process.env.MYSQL_PORT
 
 const corsOption = {
   origin:'https://netzero-cmu.web.app',
-  Credential:true,
 } 
 
-app.use((req,res,next)=>{
+/* app.use((req,res,next)=>{
   res.setHeader('Access-Control-Allow-origin','https://netzero-cmu.web.app');
   res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers','Content-Type');
   res.setHeader('Access-Control-Allow-Credentials',true)
   next()
-})
+}) */
 
 app.use(cors(corsOption)); 
 
